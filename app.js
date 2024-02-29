38de0c80bb7d69a1ce0d60f7f1d38e74f400d367
Sumit pal <tonystark83033@gmail.com>
2024-02-29 17:55:56 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { translate } = require('free-translate');
+
+const app = express()
+const PORT = 8000
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World 🎉')
+})
+
+app.get('/health',(req,res)=>{
+    res.status(200).json({
+        message:"Server is healthy"
+    })
+})
+
+
+app.post  ("/french", async (req,res)=>{
+  const text = req.body.text;
+  if(text){
+    const translatedText = await translate(text.toString(), { to: 'fr' });
+   // console.log(translation)
+    return res.status(200).json({
+      translation: translatedText
+    })
+  }else{
+    return res.status(200).json({
+      error: "No text provided in the request body"
+    })
+  }
+  
+})
+
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+})
