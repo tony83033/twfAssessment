3544faff7dc889d4c647ac658a10be42fb63b9ac
Sumit pal <tonystark83033@gmail.com>
2024-03-01 22:23:04 +0530

    fix[replace deprecated packages]

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { translate } = require('free-translate');
+const axios = require('axios');
 require('dotenv').config()
 
 const app = express()
-const PORT = process.env.PORT ||  8000
+const PORT = process.env.PORT || 8001
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -17,14 +17,42 @@ app.get('/health',(req,res)=>{
 })
 
 
-app.post  ("/french", async (req,res)=>{
+app.post  ("/french", async  (req,res)=>{
   const text = req.body.text;
   if(text){
-    const translatedText = await translate(text.toString(), { to: 'fr' });
-   // console.log(translation)
-    return res.status(200).json({
-      translation: translatedText
-    })
+    
+    const encodedParams = new URLSearchParams();
+    encodedParams.set('source_language', 'en');
+    encodedParams.set('target_language', 'fr');
+    encodedParams.set('text', text);
+    // axios options
+    const options = {
+      method: 'POST',
+      url: 'https://text-translator2.p.rapidapi.com/translate',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'X-RapidAPI-Key': 'df4a3292admsh5bcdf3a8c1f4ae1p178a58jsnc8d0a19c4f10',
+        'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+      },
+      data: encodedParams,
+    };
+
+    try {
+      const response = await axios.request(options);
+
+     // console.log(response.data);
+      return res.status(200).json({
+        translation:response.data.data.translatedText
+      })
+    } catch (error) {
+     // console.error(error);
+      return res.status(500).json({
+        error:error
+      })
+    }
+    
+    
+ 
   }else{
     return res.status(200).json({
       error: "No text provided in the request body"
